606d03b7c77f19e82ad969d8116e6ddbe7245cb0
Enes Açıkoğlu <enes.acikoglu@trendyol.com>
2018-08-09 08:39:10 +0300

    http status code fixed

--- src/main/kotlin/com/kotlin/istanbul/controller/exception/ExceptionHandlerControllerAdvice.kt
+++ src/main/kotlin/com/kotlin/istanbul/controller/exception/ExceptionHandlerControllerAdvice.kt
@@ -7,6 +7,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.MessageSource
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.util.*
@@ -18,18 +19,19 @@ class ExceptionHandlerControllerAdvice(var messageSource: MessageSource) {
     private val logger: Logger = LoggerFactory.getLogger(ExceptionHandlerControllerAdvice::class.java)
 
     @ExceptionHandler(KotlinRestApiBusinessException::class)
-    fun handleApiBusinessException(request: HttpServletRequest, kotlinRestApiBusinessException: KotlinRestApiBusinessException): ErrorDto {
+    fun handleApiBusinessException(request: HttpServletRequest, kotlinRestApiBusinessException: KotlinRestApiBusinessException): ResponseEntity<*> {
         logger.debug("kotlinRestApiBusinessException : $kotlinRestApiBusinessException")
         val message = messageSource.getMessage(kotlinRestApiBusinessException.message, null, Locale.ENGLISH)
-        return ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase, HttpStatus.INTERNAL_SERVER_ERROR.value(), message, request.requestURI, request.method)
-
+        val errorDto = ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase, HttpStatus.INTERNAL_SERVER_ERROR.value(), message, request.requestURI, request.method)
+        return ResponseEntity(errorDto, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @ExceptionHandler(KotlinApiDomainNotFoundException::class)
-    fun handleApiDomainNotFoundException(request: HttpServletRequest, kotlinApiDomainNotFoundException: KotlinApiDomainNotFoundException): ErrorDto {
+    fun handleApiDomainNotFoundException(request: HttpServletRequest, kotlinApiDomainNotFoundException: KotlinApiDomainNotFoundException): ResponseEntity<*> {
         logger.debug("kotlinRestApiBusinessException : $kotlinApiDomainNotFoundException")
         val message = messageSource.getMessage(kotlinApiDomainNotFoundException.message, null, Locale.ENGLISH)
-        return ErrorDto(HttpStatus.NOT_FOUND.reasonPhrase, HttpStatus.NOT_FOUND.value(), message, request.requestURI, request.method)
+        val errorDto = ErrorDto(HttpStatus.NOT_FOUND.reasonPhrase, HttpStatus.NOT_FOUND.value(), message, request.requestURI, request.method)
+        return ResponseEntity(errorDto, HttpStatus.NOT_FOUND)
     }
 
 
